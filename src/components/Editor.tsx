278173635cd4abeb8595dba9306b72a8ddda3baf
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2020-12-01 09:05:05 -0400

    Se puede copiar imagen al editor y se genera string HTML del contenido actual

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,18 @@
-import { FaBold, FaItalic, FaUnderline, FaQuoteLeft, FaAlignLeft, FaAlignCenter, FaListOl, FaListUl, FaLink, FaImage, FaUndo, FaRedo } from 'react-icons/Fa';
+import {
+  FaBold,
+  FaItalic,
+  FaUnderline,
+  FaQuoteRight,
+  FaAlignLeft,
+  FaAlignCenter,
+  FaListOl,
+  FaListUl,
+  FaLink,
+  FaImage,
+  FaUndo,
+  FaRedo,
+  FaHtml5,
+} from 'react-icons/Fa';
 
 declare type ToolBarOptionProps = {
   icon: JSX.Element;
@@ -11,22 +25,52 @@ declare type ToolBarSectionProps = {
 
 const Editor = () => {
   return (
-    <div className='mx-auto bg-gray-900 text-white p-2 rounded-md'>
+    <div className='mx-auto bg-gray-900 text-white p-2 rounded-md max-w-6xl'>
       <ToolBar />
       <h1 className='focus:outline-none p-2 text-white' contentEditable></h1>
-      <div className='focus:outline-none p-2 min-h-screen' contentEditable></div>
+      <div
+        id='editable'
+        className='focus:outline-none p-2 min-h-screen'
+        contentEditable
+        onPaste={(ClipEvent) => {
+          const contentTarget = document.getElementById('editable');
+
+          let cbPayload = [...(ClipEvent.clipboardData || ClipEvent.originalEvent.clipboardData).items];
+          cbPayload = cbPayload.filter((i) => /image/.test(i.type));
+
+          if (!cbPayload.length || cbPayload.length === 0) return false;
+
+          const reader = new FileReader();
+          reader.onload = (ProgresEvent) => (contentTarget.innerHTML = `<img src="${ProgresEvent.target.result}">`);
+          reader.readAsDataURL(cbPayload[0].getAsFile());
+        }}
+      ></div>
     </div>
   );
 };
 
 const ToolBar = (): JSX.Element => {
+  const getHtmlString = () => {
+    const oDoc = document.getElementById('editable');
+
+    const oContent = document.createTextNode(oDoc.innerHTML);
+
+    const oPre = document.createElement('pre');
+
+    oPre.id = 'sourceText';
+
+    oPre.appendChild(oContent);
+
+    console.log(oPre.innerHTML.toString());
+  };
+
   return (
     <div className='items-center border-b-2 border-gray-300 border-opacity-50 flex flex-wrap space-x-10'>
       <ToolBarSection>
         <ToolBarOption icon={<FaBold className='text-white' />} onClick={(e) => document.execCommand('bold', false, '')} />
         <ToolBarOption icon={<FaItalic className='text-white' />} onClick={(e) => document.execCommand('italic', false, '')} />
         <ToolBarOption icon={<FaUnderline className='text-white' />} onClick={(e) => document.execCommand('underline', false, '')} />
-        <ToolBarOption icon={<FaQuoteLeft className='text-white' />} onClick={(e) => document.execCommand('quote', false, '')} />
+        <ToolBarOption icon={<FaQuoteRight className='text-white' />} onClick={(e) => document.execCommand('formatBlock', false, '')} />
         <ToolBarOption icon={<FaAlignLeft className='text-white' />} onClick={(e) => document.execCommand('justifyLeft', false, '')} />
         <ToolBarOption icon={<FaAlignCenter className='text-white' />} onClick={(e) => document.execCommand('justifyCenter', false, '')} />
         <ToolBarOption icon={<FaListOl className='text-white' />} onClick={(e) => document.execCommand('insertOrderedList', false, '')} />
@@ -34,11 +78,23 @@ const ToolBar = (): JSX.Element => {
       </ToolBarSection>
       <ToolBarSection>
         <ToolBarOption icon={<FaLink className='text-white' />} onClick={(e) => document.execCommand('createLink', false, '')} />
-        <ToolBarOption icon={<FaImage className='text-white' />} onClick={(e) => document.execCommand('insertImage', true, '')} />
+
+        {/* <input className='hover:bg-gray-700 rounded-md focus:outline-none transition' type='file' name='file' id='uploadFile' onChange={(e) => {}} /> */}
+
+        <ToolBarOption
+          icon={<FaImage className='text-white' />}
+          onClick={(click) => {
+            document.execCommand('insertImage', true, '');
+          }}
+        />
       </ToolBarSection>
       <ToolBarSection>
         <ToolBarOption icon={<FaUndo className='text-white' />} onClick={(e) => document.execCommand('undo', false, '')} />
         <ToolBarOption icon={<FaRedo className='text-white' />} onClick={(e) => document.execCommand('redo', false, '')} />
+      </ToolBarSection>
+
+      <ToolBarSection>
+        <ToolBarOption icon={<FaHtml5 className='text-white' />} onClick={(e) => getHtmlString()} />
       </ToolBarSection>
     </div>
   );
